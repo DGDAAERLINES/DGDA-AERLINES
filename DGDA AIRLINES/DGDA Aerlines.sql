468799873366bfd489c4709e2e81c96340383c90
Danilo Zavala Meza <danilozm1997@gmail.com>
2021-03-29 00:03:33 -0600

    Prueba

--- DGDA AIRLINES/DGDA Aerlines.sql
+++ DGDA AIRLINES/DGDA Aerlines.sql
@@ -30,7 +30,7 @@ create table Usuarios.usuario(
 
 create table Aerlines.Pais (
 	id int not null,
-	nombre varchar (80),
+	nombre varchar (15),
 	constraint Pk_Pais_id
 	primary key  CLUSTERED  (id)
  )
